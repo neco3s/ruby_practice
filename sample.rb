ed4d7981191224d7aedb29d576be2acf56072181
neco3s <neco3coffee@gmail.com>
2023-04-23 08:27:47 +0900

    rubyを手を動かして学んだ軌跡

---
+++ sample.rb
@@ -0,0 +1,47 @@
+# puts "hello"
+
+# stone = 'python'
+# case stone
+#   when 'ruby'
+#     puts 'Matz'
+#   when 'python'
+#     puts 'snake'
+#   else
+#     puts 'other'
+# end
+
+# 条件分岐
+# age = 16
+# if age >=12
+#   puts '5000円'
+# elsif age >=6
+#   puts '2500円'
+# else
+#   puts '1000円'
+# end
+
+#FizzBuzz
+# def fizz_buzz(num)
+#   if num % 3 == 0 && num % 5 == 0
+#     puts 'FizzBuzz'
+#   elsif num % 3 == 0
+#     puts 'Fizz'
+#   elsif num % 5 == 0
+#     puts 'Buzz'
+#   else
+#     puts num.to_s
+#   end
+# end
+
+# fizz_buzz(15)
+# fizz_buzz(6)
+# fizz_buzz(10)
+# fizz_buzz(11)
+# fizz_buzz(208)
+
+#繰り返し処理
+i = 0
+while i < 10
+  puts "#{i}:hello"
+  i += 1
+end
